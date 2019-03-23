e0e8471d47587259a83c77a1d33a58706dd5d747
Ben Shi <ben@hbish.com>
2019-03-23 13:20:51 +1100

    chapter 20

---
+++ 20_otp_more/sequence/mix.exs
@@ -0,0 +1,36 @@
+defmodule Sequence.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :sequence,
+      version: "0.3.0",
+      elixir: "~> 1.8",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      mod: {
+        Sequence.Supervisor, []
+      },
+      env: [
+        initial_number: 456
+      ],
+      registered: [
+        Sequence.Server
+      ],
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:distillery, "~> 1.5", runtime: false}
+    ]
+  end
+end
